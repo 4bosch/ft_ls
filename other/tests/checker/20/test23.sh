d61a9082cedb1a9362ff15b44eb157173f1be12e
aboscho <argan.bosch@free.fr>
2020-02-12 17:29:17 +0100

    adding test script

---
+++ other/tests/checker/20/test23.sh
@@ -0,0 +1,10 @@
+mkdir tmp;
+cd tmp;
+echo "==== LS RESULT ====";
+ls -1 aa bb \"\";
+echo "===================\n";
+echo "=== FT_LS RESULT ==";
+ft_ls -1 aa bb \"\";
+echo "===================";
+cd .. ;
+rm -rf tmp;
---
+++ other/tests/checker/20/test24.sh
@@ -0,0 +1,10 @@
+mkdir tmp;
+cd tmp;
+echo "==== LS RESULT ====";
+ls -1 a \"\" b;
+echo "===================\n";
+echo "=== FT_LS RESULT ==";
+ft_ls -1 a \"\" b;
+echo "===================";
+cd .. ;
+rm -rf tmp;
